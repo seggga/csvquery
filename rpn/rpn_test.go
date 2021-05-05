c307e56590716db6f665686eacc234e246556432
Serg Kotovsky <info@geekserg.ru>
2021-05-05 23:11:19 +0300

    test rpn

--- rpn/rpn_test.go
+++ rpn/rpn_test.go
@@ -2,14 +2,20 @@ package rpn
 
 import "testing"
 
-func ConvertToRPNTest(t *testing.T) {
+func TestConvertToRPN(t *testing.T) {
 	query := []string{"age", ">", "40", "AND", "(", "city", "=", "Tokyo", "OR", "new", ">=", "1000", ")"}
 	expected := []string{"city", "Tokyo", "=", "new", "1000", ">=", "OR", "age", "40", ">", "AND"}
 	got := ConvertToRPN(query)
 
+	if got == nil {
+		t.Fatal("got nil-slice")
+	}
+	if len(got) != len(expected) {
+		t.Fatal("slices differ in size")
+	}
 	for i := 0; i < len(expected); i += 1 {
 		if got[i] != expected[i] {
-			t.Error("arrays differ")
+			t.Fatal("slices differ in elements")
 		}
 	}
 }
