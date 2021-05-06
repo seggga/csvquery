d69499fcd52a82c83ec932448ac100372bd477a2
Serg Kotovsky <info@geekserg.ru>
2021-05-06 17:50:31 +0300

    move split query to lexemma package

---
+++ lexemma/split_to_lexemmas.go
@@ -0,0 +1,28 @@
+package lexemma
+
+import "github.com/seggga/csvquery/token"
+
+func SplitToLexemmas(query string) []Lexemma {
+
+	var scanner token.Scanner
+	var lexx []Lexemma
+
+	fset := token.NewFileSet()
+	file := fset.AddFile("", fset.Base(), len(query))
+	scanner.Init(file, []byte(query), nil, token.ScanComments)
+
+	// run the scanner, fill the output slice
+	for {
+		_, tok, lit := scanner.Scan()
+		if tok == token.EOF {
+			break
+		}
+		if lit == "\n" {
+			continue
+		}
+
+		lexx = append(lexx, Lexemma{Token: tok, Litera: lit})
+	}
+
+	return lexx
+}
