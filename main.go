830f8a43451e35e7e2e799de153aa9a51b1f5690
Serg Kotovsky <info@geekserg.ru>
2021-05-07 13:03:39 +0300

    main  using all functions from RPN and TOKEN packages

--- main.go
+++ main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"fmt"
 
+	"github.com/seggga/csvquery/rpn"
 	"github.com/seggga/csvquery/token"
 )
 
 func main() {
 
-	// query := []string{"age", ">", "40", "AND", "(", "city", "=", "Tokyo", "OR", "new", ">=", "1000", ")"}
-	// expected := []string{"age", "40", ">", "city", "Tokyo", "=", "new", "1000", ">=", "OR", "AND"}
-	// got := rpn.ConvertToRPN(query)
-
-	// fmt.Printf("expected: %v\n", expected)
-	// fmt.Printf("     got: %v\n", got)
-
 	query := `age > 40 AND (city_name == "Tokyo" OR new_issues <= 1000)`
+	queryTokens := token.SplitQuery(query)
+	queryTokens = rpn.ConvertToRPN(queryTokens)
 
-	lexx := token.SplitQuery(query)
-	// fmt.Println(query)
-	// fmt.Printf("%+v\n", lexx)
+	valuesMap := map[string]string{
+		"age":        "30",
+		"city_name":  "Moscow",
+		"new_issues": "1000",
+	}
+	currentSlice := rpn.InsertValues(valuesMap, queryTokens)
 
-	fmt.Println(lexx)
+	got, err := rpn.CalculateRPN(currentSlice)
+
+	fmt.Println("got:", got, "error:", err)
 }
