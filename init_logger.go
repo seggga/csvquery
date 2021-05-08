644016ad3986d4b1bf961369ccb62b9ff9062ac4
Serg Kotovsky <info@geekserg.ru>
2021-05-08 15:28:44 +0300

    init logger created

---
+++ init_logger.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"github.com/seggga/csvquery/config"
+	"github.com/sirupsen/logrus"
+)
+
+// initLogInfo - initializes info logger
+func initLogInfo(log *logrus.Logger, file *os.File, conf *config.ConfigType) {
+
+	log.SetLevel(logrus.InfoLevel)
+	log.SetFormatter(&logrus.JSONFormatter{})
+	log.SetOutput(file)
+	log.Info("logging started")
+	log.Info("command-line parameters:")
+	log.Infof("timeout: %s", conf.Timeout)
+}
+
+// initLogErr - initializes error logger
+func initLogErr(log *logrus.Logger, file *os.File, conf *config.ConfigType) {
+	log.SetLevel(logrus.ErrorLevel)
+	log.SetFormatter(&logrus.JSONFormatter{})
+	log.SetOutput(file)
+}
